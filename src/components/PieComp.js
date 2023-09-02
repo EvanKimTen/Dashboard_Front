b54064d26d68893dce669ed4667dbb6445b18d76
Moonjoo Kim <124408498+hmjk99@users.noreply.github.com>
2023-09-02 18:32:25 -0400

    working on pie

--- src/components/PieComp.js
+++ src/components/PieComp.js
@@ -3,11 +3,14 @@ import { useState, useEffect } from 'react';
 
 const PieComp = () => {
     const [pieData, setPieData] = useState([])
+    const [missedCount, setMissedCount] = useState(0)
+    const [recognizedCount, setRecognizedCount] = useState(0)
 
     const getData = async () => {
         try {
           const response = await axios.post('http://localhost:5001/api/proxy/understood_messages',);
-          setPieData(response.data.result);
+          setMissedCount(response.data.result[0].missed.count);
+          setRecognizedCount(response.data.result[0].total.count - response.data.result[0].missed.count);
         } catch (error) {
           console.error(error);
         }
@@ -19,7 +22,10 @@ const PieComp = () => {
     }, []);
 
     return (
-        <div>PieComp</div>
+        <div>
+            <h1>{recognizedCount}</h1>
+            <h1>{missedCount}</h1>
+        </div>
     )
 }
 

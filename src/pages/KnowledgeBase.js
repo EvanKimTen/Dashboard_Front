798bb0c1718d70cb19e46c59d4999d7b361ff5df
jhk2463 <125618020+jhk2463@users.noreply.github.com>
2023-09-14 03:14:03 -0400

    Handling file upload

--- src/pages/KnowledgeBase.js
+++ src/pages/KnowledgeBase.js
@@ -15,7 +15,6 @@ import {
 import SearchIcon from "@mui/icons-material/Search";
 import TuneIcon from "@mui/icons-material/Tune";
 import EnhancedTable from "../components/EnhancedTable";
-import { async } from "q";
 
 const proxy = axios.create({
   baseURL: "http://localhost:5001/proxy/knowledge-base",
@@ -25,6 +24,7 @@ const KnowledgeBase = () => {
   const [documents, setDocuments] = useState([]);
   const [anchorEl, setAnchorEl] = useState(null);
   const [input, setInput] = useState("");
+
   const [openDialog, setOpenDialog] = useState(false);
 
   const open = Boolean(anchorEl);
@@ -67,6 +67,27 @@ const KnowledgeBase = () => {
     }
   };
 
+  const handleFileUpload = async (event) => {
+    try {
+      const formData = new FormData();
+      formData.append("file", event.target.files[0]);
+      formData.append("fileName", event.target.files[0].name);
+      console.log(event.target.files[0]);
+      for (const value of formData.values()) {
+        console.log(value);
+      }
+      const config = {
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+      };
+      const response = await proxy.post("/file", formData, config);
+      console.log(response.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div>
       <TopBar>
@@ -102,10 +123,31 @@ const KnowledgeBase = () => {
             }}
           >
             <MenuItem onClick={handleUrlClick}>URL(s)</MenuItem>
-            <MenuItem onClick={handleClose}>Sitemap</MenuItem>
-            <MenuItem onClick={handleClose}>Text</MenuItem>
-            <MenuItem onClick={handleClose}>PDF</MenuItem>
-            <MenuItem onClick={handleClose}>DOC</MenuItem>
+            <MenuItem onClick={handleUrlClick}>Sitemap</MenuItem>
+            <MenuItem component="label">
+              Text
+              <VisuallyHiddenInput
+                type="file"
+                accept=".txt"
+                onChange={handleFileUpload}
+              />
+            </MenuItem>
+            <MenuItem component="label">
+              PDF
+              <VisuallyHiddenInput
+                type="file"
+                accept=".pdf"
+                onChange={handleFileUpload}
+              />
+            </MenuItem>
+            <MenuItem component="label">
+              DOC
+              <VisuallyHiddenInput
+                type="file"
+                accept=".doc, .docx"
+                onChange={handleFileUpload}
+              />
+            </MenuItem>
           </Menu>
         </Buttons>
       </TopBar>
@@ -176,6 +218,18 @@ const Buttons = styled.div`
   justify-content: center;
   align-items: center;
   gap: 0.5rem;
+`;
+
+const VisuallyHiddenInput = styled("input")`
+  clip: rect(0 0 0 0);
+  clip-path: inset(50%);
+  height: 1px;
+  overflow: hidden;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  white-space: nowrap;
+  width: 1px;
 `;
 
 export default KnowledgeBase;
